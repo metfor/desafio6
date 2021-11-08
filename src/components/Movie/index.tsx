f7a45520e1c0f1e2fcc579d711371050482b41be
metfor <mateus_frade@hotmail.com>
2021-11-08 11:30:34 -0300

    finish

---
+++ src/components/Movie/index.tsx
@@ -0,0 +1,93 @@
+import {
+  BoxButton,
+  BoxDescription,
+  BoxEvaluation,
+  BoxImg,
+  Container,
+  ContainerDesc,
+  Description,
+  Img,
+  ImgMovie,
+  InfoMovie,
+  InfoP,
+  Title,
+  Trailer,
+  Warning,
+  WatchNow,
+} from "./styled";
+import { BsClock } from "react-icons/bs";
+export function Movie() {
+  return (
+    <Container>
+      <Warning>ESTÁ DISPONÍVEL AGORA!</Warning>
+      <Title>LOKI</Title>
+      <BoxDescription>
+        <ImgMovie
+          className="movie"
+          src="/../public/capa.svg"
+          alt="movie"
+          width="184"
+          height="267"
+        />
+        <ContainerDesc>
+          <Description>
+            Em “Loki” da Marvel Studios, o vilão mercurial Loki (Tom Hiddleston)
+            <br /> retoma seu papel como o Deus do Mal em uma nova série que
+            ocorre após <br />
+            os eventos de “Vingadores: Endgame”.
+          </Description>
+          <InfoMovie>
+            <BoxImg>
+              <Img
+                className="clock"
+                src="/../public/clock.svg"
+                width="25"
+                height="25"
+              />
+              <InfoP>51min</InfoP>
+            </BoxImg>
+            <BoxEvaluation>
+              <Img
+                className="star"
+                src="/../public/star.svg"
+                width="25"
+                height="25"
+              />
+              <Img
+                className="star"
+                src="/../public/star.svg"
+                width="25"
+                height="25"
+              />
+              <Img
+                className="star"
+                src="/../public/star.svg"
+                width="25"
+                height="25"
+              />
+              <Img
+                className="star"
+                src="/../public/star.svg"
+                width="25"
+                height="25"
+              />
+              <Img
+                className="Shadow"
+                src="/../public/StarShadow.svg"
+                width="25"
+                height="25"
+              />
+            </BoxEvaluation>
+            <BoxImg>
+              <InfoP>2021</InfoP>
+            </BoxImg>
+          </InfoMovie>
+          <BoxButton>
+            <WatchNow> Assistir Agora</WatchNow>
+            <Trailer> Trailer</Trailer>
+          </BoxButton>
+        </ContainerDesc>
+      </BoxDescription>
+    </Container>
+  );
+}
---
+++ src/components/Movie/styled.ts
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import image from "next/image";
+export const Warning = styled.h1`
+  font-family: B612;
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+  color: white;
+  margin-top: 2.8125rem;
+  text-align: center;
+`;
+export const Title = styled.h1`
+  font-family: B612;
+  font-weight: 700;
+  font-size: 6rem;
+  line-height: 7.25rem;
+  color: white;
+  text-align: center;
+`;
+export const Description = styled.p`
+  font-family: B612;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 2rem;
+  color: white;
+`;
+export const Container = styled.div`
+  display: grid;
+  justify-content: center;
+  grid-template-rows: 65px 280px;
+`;
+export const BoxDescription = styled.div`
+  display: grid;
+  grid-template-columns: 200px 820px;
+`;
+export const ImgMovie = styled(image)``;
+export const ContainerDesc = styled.div`
+  margin-left: 73px;
+  display: grid;
+`;
+export const InfoMovie = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 600px;
+`;
+export const Img = styled(image)``;
+export const BoxImg = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const InfoP = styled.p`
+  font-family: B612;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.8125rem;
+  color: white;
+  margin-left: 8px;
+`;
+export const BoxEvaluation = styled.div`
+  display: grid;
+  grid-template-columns: 25px 25px 25px 25px 25px;
+  grid-column-gap: 15px;
+`;
+export const WatchNow = styled.button`
+  font-weight: 700;
+  background: #0072d2;
+  border-radius: 3px;
+  width: 184px;
+  height: 52px;
+  color: white;
+  font-family: B612;
+  border: none;
+`;
+export const BoxButton = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+`;
+export const Trailer = styled.button`
+  font-weight: 700;
+  background: #131318;
+  border: 1px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 3px;
+  width: 184px;
+  height: 52px;
+  color: white;
+  font-family: B612;
+  border: none;
+`;
